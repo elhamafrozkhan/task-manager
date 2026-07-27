65f4c72201e8313652bcf2b1059232c86489cfff
elham <elham.a.khan@gmail.com>
2026-07-27 13:35:04 +0200

    add profile page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,9 @@
 import {createRouter, createWebHistory} from 'vue-router';
-import Dashboard from '../components/Dashboard.vue'
-import Login from '../components/Login.vue'
-import { useAuthStore } from '../stores/auth'
-import Register from '../components/Register.vue'
+import Dashboard from '../components/Dashboard.vue';
+import Login from '../components/Login.vue';
+import { useAuthStore } from '../stores/auth';
+import Register from '../components/Register.vue';
+import Profile from '../components/Profile.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -18,7 +19,12 @@ const router = createRouter({
             {
             path: '/register',
             component: Register
+            },
+            {
+            path: '/profile',
+            component: Profile
             }
+            
         ]
 })
 router.beforeEach((to, from) => {
